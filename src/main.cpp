456b3435185525a68fc6ee25eab115d3e94dc6b9
Nicolas Velasquez <gnicolax@gmail.com>
2014-06-06 20:39:41 +0200

    Init OpenGL before Kalman Filter

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,14 @@
 #include "ekfoa.hpp"
 
 int main(int argc, char** argv){
+	//initialize the OpenGL gui:
+	Gui::init();
 
 	//Start a thread for the Extended Kalman Filter:
 	EKFOA ekfoa;
     boost::thread ekfoa_thread (&EKFOA::start, &ekfoa);
 
-	//initialize the OpenGL gui:
-	Gui::init();
-
 	bool keep_going = true;
-
     while (keep_going){
     	keep_going = Gui::redraw();
     }
