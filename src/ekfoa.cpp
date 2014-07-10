0405f8661bc662112a10ea4498d60c02504d9c36
Nicolas Velasquez <gnicolax@gmail.com>
2014-07-10 15:10:12 +0200

    Better current position and orientation matrix names, set to rW and qWR_R

--- src/ekfoa.cpp
+++ src/ekfoa.cpp
@@ -43,7 +43,7 @@ motion_tracker(MotionTrackerOF(
 
 }
 
-void EKFOA::process(const double delta_t, cv::Mat & frame, Eigen::Vector3d & current_position, Eigen::Matrix3d & axes_orientation_and_confidence, std::vector<Point3d> (& XYZs)[3], Delaunay & triangulation, Point3d & closest_point){
+void EKFOA::process(const double delta_t, cv::Mat & frame, Eigen::Vector3d & rW, Eigen::Matrix3d & axes_orientation_and_confidence, std::vector<Point3d> (& XYZs)[3], Delaunay & triangulation, Point3d & closest_point){
 	double time;
 	std::vector<cv::Point2f> features_to_add;
 	std::vector<Features_extra> features_extra;
@@ -99,20 +99,20 @@ void EKFOA::process(const double delta_t, cv::Mat & frame, Eigen::Vector3d & cur
 	const Eigen::MatrixXd & p_k_k = filter.p_k_k();
 
 	//Set the position, so the GUI can draw it:
-	current_position = x_k_k.segment<3>(0);
+	rW = x_k_k.segment<3>(0);//current position
 
 	//Set the axes orientation and confidence:
 	axes_orientation_and_confidence.setIdentity();//axes_orientation_and_confidence stores in each column one axis (X, Y, Z)
 	axes_orientation_and_confidence *= 2; //make the lines larger, so they are actually informative
 	//Apply rotation matrix:
-	Eigen::Matrix3d orientation_R;
-	MotionModel::quaternion_matrix(x_k_k.segment<4>(3), orientation_R);
-	axes_orientation_and_confidence.applyOnTheLeft(orientation_R); // == R * axes_orientation_and_confidence
+	Eigen::Matrix3d qWR_R;//Rotation matrix of current orientation quaternion
+	MotionModel::quaternion_matrix(x_k_k.segment<4>(3), qWR_R);
+	axes_orientation_and_confidence.applyOnTheLeft(qWR_R); // == R * axes_orientation_and_confidence
 	for (int axis=0 ; axis<axes_orientation_and_confidence.cols() ; axis++){
 		//Set the length to be 3*sigma:
 		axes_orientation_and_confidence.col(axis) *= 3*std::sqrt(p_k_k(axis, axis)); //the first 3 positions of the cov matrix define the confidence for the position
 		//Translate origin:
-		axes_orientation_and_confidence.col(axis) += current_position;
+		axes_orientation_and_confidence.col(axis) += rW;
 	}
 
 	int num_features = (x_k_k.rows()-13)/6;
@@ -137,9 +137,9 @@ void EKFOA::process(const double delta_t, cv::Mat & frame, Eigen::Vector3d & cur
 		point_close(5) += sigma_3;
 		point_far(5) -= sigma_3;
 
-		Eigen::Vector3d XYZ_mu = (Feature::compute_cartesian(yi) - x_k_k.head(3) + current_position); //mu (mean)
-		Eigen::Vector3d XYZ_close = (Feature::compute_cartesian(point_close) - x_k_k.head(3) + current_position); //mean + 3*sigma. (since inverted signs are also inverted)
-		Eigen::Vector3d XYZ_far = (Feature::compute_cartesian(point_far) - x_k_k.head(3) + current_position); //mean - 3*sigma
+		Eigen::Vector3d XYZ_mu = (Feature::compute_cartesian(yi)); //mu (mean)
+		Eigen::Vector3d XYZ_close = (Feature::compute_cartesian(point_close)); //mean + 3*sigma. (since inverted signs are also inverted)
+		Eigen::Vector3d XYZ_far = (Feature::compute_cartesian(point_far)); //mean - 3*sigma
 
 		//The center of the model is ALWAYS the current position of the camera/robot, so have to 'cancel' the current orientation (R_inv) and translation (rWC = x_k_k.head(3)):
 		//Note: It is nicer to do this in the GUI class, as it is only a presention/perspective change. But due to the structure, it was easier to do it here.
@@ -178,7 +178,7 @@ void EKFOA::process(const double delta_t, cv::Mat & frame, Eigen::Vector3d & cur
 
 	if (tree.size()>0){
 		// compute closest point and squared distance
-		Point3d point_query(current_position[0], current_position[1], current_position[2]);
+		Point3d point_query(rW[0], rW[1], rW[2]);
 		closest_point = tree.closest_point(point_query);
 //		FT sqd = tree.squared_distance(point_query);
 	}
